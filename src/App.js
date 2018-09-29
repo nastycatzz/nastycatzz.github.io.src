f29c66dc60b78d9a4cdf4755b42b8bcbe35b40c0
Nasty Catzz <nastycatzz@reddithub.com>
2018-09-29 04:28:47 +0000

    add calendar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Calendar, CalendarControls} from 'react-yearly-calendar';
+import './Calendar.css';
+
+function onDatePicked(date) {
+  alert(date);
+}
 
 class App extends Component {
   render() {
@@ -8,11 +14,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Nasty Catzz</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Calendar year={2018} onPickDate={onDatePicked} />
       </div>
     );
   }
